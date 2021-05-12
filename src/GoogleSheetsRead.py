cb1960add24290aca99f7c7cd4d3e0b44757a565
Norman <norman_cade32@yahoo.com>
2021-05-12 18:16:41 -0400

    Add color to cells depending on 50 and 200 day avg

--- src/GoogleSheetsRead.py
+++ src/GoogleSheetsRead.py
@@ -1,26 +1,50 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-scope = ['https://www.googleapis.com/auth/spreadsheets',
-'https://www.googleapis.com/auth/drive']
+def open_worksheet(sheetName) :
+    scope = ['https://www.googleapis.com/auth/spreadsheets',
+        'https://www.googleapis.com/auth/drive']
 
-creds = ServiceAccountCredentials.from_json_keyfile_name("robinhood-webscraper-key.json", scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name("robinhood-webscraper-key.json", scope)
 
-client = gspread.authorize(creds)
+    client = gspread.authorize(creds)
 
-sheet = client.open("Robinhood-Webscraper").sheet1
+    sheet = client.open(sheetName).sheet1
+    return sheet
 
-data = sheet.get_all_records()
+def populate_table (sheet, collected_stock_info) :
+    rowNum = 2
+    sheet.delete_rows(rowNum, (len(collected_stock_info) + rowNum))
+    print("Rows cleared")
 
-row = sheet.row_values(3)
-col = sheet.col_values(2)
-cell = sheet.cell(1,2).value
+    for stock in collected_stock_info :
+        insertRow = [stock.symbol, stock.price, stock.closing_price, stock.sector, stock.fifty_day_avg, stock.twohundred_day_avg]
+        sheet.insert_row(insertRow,rowNum)
+        _pick_cell_color(rowNum, stock, sheet)
+        rowNum = rowNum + 1
+    print("Rows updated")
 
-# insertRow = ["hello", 5, "red", "blue"]
-# sheet.insert_row(insertRow, 4)
-# sheet.delete_row(4)
-# sheet.update_cell(2, 2, "Changed")
 
-numRows = len(data)
+# ------------ Helper Functions ---------------
 
-print(numRows)
+def _pick_cell_color(rowNum, stock, sheet) :
+    if stock.fifty_day_avg > stock.price :
+        sheet.format("E" + str(rowNum), {
+            "backgroundColor": {
+            "red": 1.0,
+        }}) 
+    else :
+        sheet.format("E" + str(rowNum), {
+            "backgroundColor": {
+            "green": 1.0,
+        }})
+    if stock.twohundred_day_avg > stock.price :
+        sheet.format("F" + str(rowNum), {
+            "backgroundColor": {
+            "red": 1.0,
+        }}) 
+    else :
+        sheet.format("F" + str(rowNum), {
+            "backgroundColor": {
+            "green": 1.0,
+        }})
--- src/Main.py
+++ src/Main.py
@@ -9,6 +9,7 @@ from collections import namedtuple
 import GetStockInfo
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import GoogleSheetsRead
 import yaml
 
 # Get neccessary credentials
@@ -17,46 +18,24 @@ email = conf['user']['email']
 pwd = conf['user']['password']
 key = conf['user']['key']
 
+def main():
+    today = date.today()
 
-today = date.today()
+    try :
+        # Login into Robinhood
+        totp = pyotp.TOTP(key).now()
+        login = rb.login(email, pwd, mfa_code=totp)
+    except :
+        print("Error logging into to Robinhood")
 
-try :
-    # Login into Robinhood
-    totp = pyotp.TOTP(key).now()
-    login = rb.login(email, pwd, mfa_code=totp)
-except :
-    print("Error logging into to Robinhood")
-
-myStocks = GetStockInfo.build_portfolio()
-collected_stock_info = GetStockInfo.collect_stock_info(myStocks)
-
-scope = ['https://www.googleapis.com/auth/spreadsheets',
-'https://www.googleapis.com/auth/drive']
-
-creds = ServiceAccountCredentials.from_json_keyfile_name("robinhood-webscraper-key.json", scope)
-
-client = gspread.authorize(creds)
-
-sheet = client.open("Robinhood-Webscraper").sheet1
-
-data = sheet.get_all_records()
+    myStocks = GetStockInfo.build_portfolio()
+    collected_stock_info = GetStockInfo.collect_stock_info(myStocks)
+    sheet = GoogleSheetsRead.open_worksheet("Robinhood-Webscraper")
+    GoogleSheetsRead.populate_table(sheet, collected_stock_info)
 
 
-
-
-
-
-rowNum = 2
-sheet.delete_rows(rowNum, (len(collected_stock_info) + rowNum))
-print("Rows cleared")
-
-for stock in collected_stock_info :
-    insertRow = [stock.symbol, stock.price, stock.closing_price, stock.sector, stock.fifty_day_avg, stock.twohundred_day_avg]
-    sheet.insert_row(insertRow,rowNum)
-    rowNum = rowNum + 1
-print("Rows updated")
-
-
+if __name__ == "__main__":
+    main()
 
 # dateList = stocks["stock_previous_close_dates"][0].split("-")
 # stringToDate = date(int(dateList[0]), int(dateList[1]), int(dateList[2]))
