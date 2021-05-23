ea600440210accacf3e8a332fe9b810433ede156
Norman <norman_cade32@yahoo.com>
2021-05-23 11:54:29 -0400

    populated total statistics

--- src/GoogleSheetsRead.py
+++ src/GoogleSheetsRead.py
@@ -2,6 +2,8 @@ from typing import Dict
 from openpyxl import load_workbook
 from openpyxl.styles import Font, Border, Side
 from collections import Counter
+import robin_stocks.robinhood as rb
+
 
 from openpyxl.styles.borders import BORDER_THIN
 
@@ -9,6 +11,7 @@ from openpyxl.styles.borders import BORDER_THIN
 # Format rule for currency
 FORMAT_CURRENCY_USD_SIMPLE = '"$"#,##0.00_-'
 NUMBER_WITH_DECIMAL = '#,##0.00'
+
 
 BORDER_FORMAT = Border(top = Side(border_style='thin', color='FF000000'),    
                               right = Side(border_style='thin', color='FF000000'), 
@@ -65,17 +68,28 @@ def populate_table (workbookPath, collected_stock_info) :
             ws.append(insertRow)
             _pick_cell_color(startRowNum, len(insertRow), stock, ws)
             startRowNum = startRowNum + 1
-
-        ws["E" + str(amountOfStocks + 3)] = "Total"
+        portfolio_equity = float(rb.account.load_phoenix_account(info="portfolio_equity")["amount"])
+        ws["E" + str(amountOfStocks + 3)] = "Total Amount Spent"
         ws["E" + str(amountOfStocks + 3)].font = Font(bold=True)
         ws["F" + str(amountOfStocks + 3)] = amount_spent_total
 
+        ws["E" + str(amountOfStocks + 4)] = "Total Portfolio Equity"
+        ws["E" + str(amountOfStocks + 4)].font = Font(bold=True)
+        ws["F" + str(amountOfStocks + 4)] = portfolio_equity
+
+        ws["E" + str(amountOfStocks + 5)] = "Profit"
+        ws["E" + str(amountOfStocks + 5)].font = Font(bold=True)
+        ws["F" + str(amountOfStocks + 5)] = portfolio_equity - amount_spent_total
+        ws["F" + str(amountOfStocks + 5)].style = "Good"
+        
         _populate_sector_chart(sheet=ws, sectors=listOfSectors, amountOfStocks=amountOfStocks)
         print("Rows updated")
 
         # Format range of cells to be currency
         range1 = ws["C2" : "D" + str(amountOfStocks + 1)]
-        range2 = ws["F2" : "I" + str(amountOfStocks + 3)]
+        range2 = ws["F2" : "I" + str(amountOfStocks + 5)]
+        range3 = ws["E2" : "E" + str(amountOfStocks + 1)]
+        _format_cells(range3, format=NUMBER_WITH_DECIMAL)
         _format_cells(range1, format=FORMAT_CURRENCY_USD_SIMPLE)
         _format_cells(range2, format=FORMAT_CURRENCY_USD_SIMPLE)
     except Exception as ex :
@@ -87,15 +101,14 @@ def populate_table (workbookPath, collected_stock_info) :
         if(ex == "PermissionError") :
             print("Make sure excel sheet is closed before running program")
     finally :
-        wb.save(r"/Users/normancade/Desktop/Robinhood-Webscraper.xlsx")
-        # wb.save(r"C:\Users\Dj\Desktop\Robinhood-Webscraper.xlsx")
+        wb.save(workbookPath)
 
 
 # ------------ Helper Functions ---------------
 
 def _populate_sector_chart(sheet, sectors, amountOfStocks) :
     dictSector = Counter(sectors)
-    rowNum = amountOfStocks + 5
+    rowNum = amountOfStocks + 7
     lastStockCell = amountOfStocks + 1
     range = sheet["G" + str(rowNum) : "I" + str(rowNum + len(dictSector))]
     _border_cells(range= range)
@@ -106,11 +119,20 @@ def _populate_sector_chart(sheet, sectors, amountOfStocks) :
 
     i = rowNum + 1
     for sector in dictSector.items() :
+        stocksInEachSector = '=COUNTIF(B2:B{lastStockCell}, "{sector}")'.format(lastStockCell= lastStockCell, sector= sector[0])
+        sharesInEachSector = '=SUMIF(B2:B{lastStockCell},G{row},E2:E{lastStockCell})'.format(lastStockCell= lastStockCell, row= i)
         sheet.cell(row= i, column= 7, value= sector[0])
-        sheet.cell(row= i, column= 8, value= '=COUNTIF(B2:B{lastStockCell}, "{sector}")'.format(lastStockCell= lastStockCell, sector= sector[0]))
-        sheet.cell(row= i, column= 9, value= '=SUMIF(B2:B{lastStockCell},G{row},E2:E{lastStockCell})'.format(lastStockCell= lastStockCell, row= i)).number_format = NUMBER_WITH_DECIMAL
+        sheet.cell(row= i, column= 8, value= stocksInEachSector)
+        sheet.cell(row= i, column= 9, value= sharesInEachSector).number_format = NUMBER_WITH_DECIMAL
         i = i + 1
+        
+    sheet["G" + str(i + 1)] = "Total of Stocks"
+    sheet["G" + str(i + 1)].font = Font(bold=True)
+    sheet.cell(row= i + 1, column= 8, value= '=SUM(H{startCell}:H{endCell})'.format(startCell= rowNum + 1, endCell= rowNum + 1 + len(dictSector)))
 
+    sheet["G" + str(i + 2)] = "Total of Shares"
+    sheet["G" + str(i + 2)].font = Font(bold=True)
+    sheet.cell(row= i + 2, column= 8, value= '=SUM(I27:I34)').number_format = NUMBER_WITH_DECIMAL
 
 def _border_cells(range) :
     for cellTuple in range :
--- src/Main.py
+++ src/Main.py
@@ -9,23 +9,23 @@ conf = yaml.load(open('credentials.yml'), Loader=yaml.FullLoader)
 email = conf['user']['email']
 pwd = conf['user']['password']
 key = conf['user']['key']
+workbookPath = conf['user']['workbookPath']
+
 
 def main():
 
     try :
         # Login into Robinhood
         totp = pyotp.TOTP(key).now()
-        login = rb.login(email, pwd, mfa_code=totp)
+        rb.login(email, pwd, mfa_code=totp)
     except Exception as ex :
         print("Error: Cannot log into to Robinhood")
         template = "An exception of type {0} occurred. Arguments:\n{1!r}"
         message = template.format(type(ex).__name__, ex.args)
         print(message)
-
     myStocks = GetStockInfo.build_portfolio()
     collected_stock_info = GetStockInfo.collect_stock_info(myStocks)
-    GoogleSheetsRead.populate_table(r"/Users/normancade/Desktop/Robinhood-Webscraper.xlsx", collected_stock_info)
-    # GoogleSheetsRead.populate_table(r"C:\Users\Dj\Desktop\Robinhood-Webscraper.xlsx", collected_stock_info)
+    GoogleSheetsRead.populate_table(workbookPath, collected_stock_info)
 
 
 if __name__ == "__main__":
