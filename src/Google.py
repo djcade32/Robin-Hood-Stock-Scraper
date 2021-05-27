ac5dcfef4c6d1d7b250fd02e76b49a5f3e76e233
Norman Cade <norman_cade32@yahoo.com>
2021-05-27 15:36:22 -0400

    Trying to delete excel sheet from google drive

--- src/Google.py
+++ src/Google.py
@@ -1,9 +1,9 @@
 import json
 import requests
 from requests.api import delete
-headers = {"Authorization": "Bearer ya29.a0AfH6SMC0WXdco4GAIsW0x7CdEN0z856iOF1aswOZpiUIvX0B-lxJ6vFmDqkwfOFgWKnc8W3WdwLjnz-Ju_CfkLqCY2uk3fVICC_7R1u91EUg263u0l71T7KfwTByvIfG1JbqDnElv_j37nMhJsj1M2Hh_Mo4"}
+headers = {"Authorization": "Bearer ya29.a0AfH6SMBA5TgYao98_KzE8eNEMyjivfd32dPVtzt0krWYxTxVCgsKT5qzJQFMypp7bbSw5jOg8RA_yHdKW1HL455pbL6JT8CDKnymTn-g0SKDDCV4IvU9dNNBDhmBk_xQ96jDZoe9rARnqoccs2cRMoHn2hBy"}
 para = {
-    "name": "sample.xlsx",
+    "name": "sample2.xlsx",
 }
 files = {
     'data': ('metadata', json.dumps(para), 'application/json; charset=UTF-8'),
@@ -14,10 +14,40 @@ r = requests.post(
     headers=headers,
     files=files
 )
+print("added")
 
-requests.delete(
-    "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
-
+p = requests.delete(
+    "https://www.googleapis.com/drive/v3/files/" + "1kjWWrUiE6xxVivTXcJA1eavcMvQ2Ei5I",
+    headers=headers,
 )
 
+print("deleted")
+
+# requests.delete(
+#     "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
+
+# )
+
 print(r.text)
+
+# from pydrive.auth import GoogleAuth
+# from pydrive.drive import GoogleDrive
+
+# gauth = GoogleAuth()
+# gauth.LocalWebserverAuth() # client_secrets.json need to be in the same directory as the script
+# drive = GoogleDrive(gauth)
+
+# fileList = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+# for file in fileList:
+#   print('Title: %s, ID: %s' % (file['title'], file['id']))
+#   # Get the folder ID that you want
+#   if(file['title'] == "Stocks"):
+#       fileID = file['id']
+
+# file1 = drive.CreateFile({"mimeType": "text/csv", "parents": [{"kind": "drive#fileLink", "id": fileID}]})
+# file1.SetContentFile("small_file.csv")
+# file1.Upload() # Upload the file.
+# print('Created file %s with mimeType %s' % (file1['title'], file1['mimeType']))   
+
+
+
